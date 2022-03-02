288f827e3eed14763000080db164f958f8761033
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-03-02 14:24:05 +0900

    GET -> 페이징 반환으로 변경

--- src/main/java/com/example/gradlespringboottest/controller/PostController.java
+++ src/main/java/com/example/gradlespringboottest/controller/PostController.java
@@ -3,16 +3,18 @@ package com.example.gradlespringboottest.controller;
 import com.example.gradlespringboottest.domain.Post;
 import com.example.gradlespringboottest.repository.PostRepositroy;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class PostController {
+
+    private static final Integer PAGE_SIZE = 20;
 
     public final PostRepositroy postRepositroy;
 
@@ -21,8 +23,16 @@ public class PostController {
         return postRepositroy.save(post);
     }
 
-    @GetMapping("/posts")
+/*    @GetMapping("/posts")
     public List<Post> getPostList() {
         return postRepositroy.findAll();
+    }*/
+
+    // 글 목록 페이징하여 반환
+    @GetMapping("/posts")
+    public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
+        return postRepositroy.findAll(
+                PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
+        );
     }
 }
